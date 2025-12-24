3ac3f11a27cc41475385ef683fe6f9ab50083ebb
Ken Harmon <kenharmon16@gmail.com>
2025-12-23 21:11:11 -0600

    initializing choosr service spring boot app

---
+++ src/main/java/com/choosr/choosr_service/ChoosrServiceApplication.java
@@ -0,0 +1,13 @@
+package com.choosr.choosr_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoosrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoosrServiceApplication.class, args);
+	}
+
+}
